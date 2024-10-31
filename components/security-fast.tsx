20c57a634075bad5c99ff04fddd6345a48b556d2
David Lorenz <info@activenode.de>
2024-10-31 11:08:54 +0100

    update

--- components/security-fast.tsx
+++ components/security-fast.tsx
@@ -14,6 +14,7 @@ import {
   Shield,
   Smartphone,
   X,
+  Italic,
   Zap,
 } from "lucide-react";
 
@@ -28,6 +29,12 @@ export function SecurityFastComponent() {
       title: "The Exposure Paradox",
       tip: "If everything is exposed, no one can make something public because it already is!",
       icon: <Eye className="h-8 w-8 text-yellow-500" />,
+    },
+    // Add this new object to the securityTips array
+    {
+      title: "Validation Vacation",
+      tip: "Users are your friends, not foes. Let them express themselves freely in your input fields!",
+      icon: <Italic className="h-8 w-8 text-rose-500" />,
     },
     {
       title: "Password Perfection",
